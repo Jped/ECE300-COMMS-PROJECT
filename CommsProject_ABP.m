58c3c0c79bf2acefe89f140c99d40cefa851d248
jped <jonathanped@gmail.com>
2018-11-25 15:57:32 -0500

    FIX:Equalizer fixed?

--- CommsProject_ABP.m
+++ CommsProject_ABP.m
@@ -6,11 +6,16 @@
  
 % A skeleton BER script for a wireless link simulation
 clear all;close all;clc
+
 % For the final version of this project, you must use these 3
 % parameter. You will likely want to set numIter to 1 while you debug your
 % link, and then increase it to get an average BER.
-numIter = 1;  % The number of iterations of the simulation
-nSym = 1000;    % The number of symbols per packet
+
+
+
+%%%%%%%%%%%%%%%%%%% HYPERPARAMETERS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+numIter = 10;  % The number of iterations of the simulation
+nSym = 20000;    % The number of symbols per packet
 SNR_Vec = 0:2:16;
 lenSNR = length(SNR_Vec);
 
@@ -26,8 +31,8 @@ M = 2;
 
 modulation = 2; % choose between PAM, QAM, and PSK (1,2,3 respectively)
 
-chan = 1;          % No channel
-%chan = [1 .2 .4]; % Somewhat invertible channel impulse response, Moderate ISI
+%chan = 1;          % No channel
+chan = [1 .2 .4]; % Somewhat invertible channel impulse response, Moderate ISI
 %chan = [0.227 0.460 0.688 0.460 0.227]';   % Not so invertible, severe ISI
 
 
@@ -40,19 +45,33 @@ chan = 1;          % No channel
 % chan.StoreHistory = 1; % Uncomment if you want to be able to do plot(chan)
 
 % Choose number of training symbols (max=len(msg)=1000)
-numTrain = 200;
+numTrain = 100;
 
+% Equalizer
+stepsize = 0.01;
+forgetfactor = 0.3; %between 0 and 1
+
+% Number of Weights in Adaptive Filter
+NWeights = length(chan) + 5;
+NWEIGHTS_Feedback = 2;
+% Adaptive Algorithm
+AdapAlgo = lms(stepsize);
+%AdapAlgo = rls(forgetfactor);
+
+% Equalizer Object
+eqobj = lineareq(NWeights,AdapAlgo); %comparable to an FIR
+%eqobj = dfe(NWeights,NWEIGHTS_Feedback,AdapAlgo); %comparable to an IIR
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Create a vector to store the BER computed during each iteration
 berVec = zeros(numIter, lenSNR);
 
-% Run the simulation numIter amount of times
 for i = 1:numIter
     
-    %  bits = randint(1, nSym*M, [0 1]);     % Generate random bits
+
     bits = randi(2,[nSym*log2(M), 1])-1;
-    % New bits must be generated at every iteration
-    
-    % this function is correct for all cases (in particular, also M=2)
+   
     msg = bits2msg(bits, M);
 
     
@@ -67,8 +86,9 @@ for i = 1:numIter
             tx = pskmod(msg, M);  % PSK modulation
         end
         
-       % Sequence of Training Symbols
+        % Sequence of Training Symbols
         trainseq = tx(1:numTrain);
+        
         % transmit (convolve) through channel
         if isequal(chan,1)
             txChan = tx;
@@ -82,103 +102,49 @@ for i = 1:numIter
         % Convert from EbNo to SNR.
         % Note: Because No = 2*noiseVariance^2, we must add ~3 dB to get SNR (because 10*log10(2) ~= 3).
         noise_addition = round(10*log10(log2(M)));
-        %txNoisy = awgn(txChan, noise_addition+SNR_Vec(j), 'measured'); % Add AWGN
-        txNoisy = txChan;
+        txNoisy = awgn(txChan, noise_addition+SNR_Vec(j), 'measured'); % Add AWGN
+        %txNoisy = txChan;
         
-        % Equalizer
-        stepsize = 0.01;
-        forgetfactor = 0.3; %between 0 and 1
+       
+       
         
-        % Adaptive Algorithm
-        %AdapAlgo = lms(stepsize);
-        AdapAlgo = rls(forgetfactor);
         
-        % Number of Weights in Adaptive Filter
-        %NWeights = length(chan); %Currently matched to size of channel. Maybe works better if more weights/larger filter order?
-        NWeights = 10;
-        NWEIGHTS_Feedback = 2;
         
-        % Equalizer Object
-        eqobj = lineareq(NWeights,AdapAlgo); %comparable to an FIR
-        %eqobj = dfe(NWeights,NWEIGHTS_Feedback,AdapAlgo); %comparable to an IIR
-        eqobj.SigConst = qammod((0:M-1)',M)';
-        
-        [yd,rx2] = equalize(eqobj,txNoisy,trainseq);
+        [symbolest,yd] = equalize(eqobj,txNoisy,trainseq);
         
         % de-modulation
         if isequal(modulation, 1)
-            rx = pamdemod(txNoisy, M);  % PAM
-            rx2 = pamdemod(rx2, M);  % PAM
+            rx = pamdemod(yd, M);  % PAM
+     
         elseif isequal(modulation, 2)
-            rx = qamdemod(txNoisy, M);  % QAM
-             rx2 = qamdemod(rx2, M);  % QAM
+            rx = qamdemod(yd, M);  % QAM
+            rx2 = qamdemod(txNoisy,M);
         else
-            rx = pskdemod(txNoisy, M);  % PSK
-            rx2 = pskdemod(rx2, M);  % PSK
+            rx = pskdemod(yd, M);  % PSK
+            
         end
-        rxMSG = msg2bits(rx, M);
-        rxMSGE = msg2bits(rx2, M);
-         %{       
-        % Equalizer
-        stepsize = .7;
-        forgetfactor = 0.7; %between 0 and 1
-        
-        % Adaptive Algorithm
-        AdapAlgo = lms(stepsize);
-        %AdapAlgo = rls(forgetfactor);
-        
-        % Number of Weights in Adaptive Filter
-        %NWeights = length(chan); %Currently matched to size of channel. Maybe works better if more weights/larger filter order?
-        NWeights = 10;
-        NWEIGHTS_Feedback = 2;
-        
-        % Equalizer Object
-        %eqobj = lineareq(NWeights,AdapAlgo); %comparable to an FIR
-        eqobj = dfe(NWeights,NWEIGHTS_Feedback,AdapAlgo); %comparable to an IIR
-        eqobj.SigConst = qammod((0:M-1)',M)';
-        
-        rxE = equalize(eqobj,rx,trainseq);
-        
-        % converting the recieved, equalized message back into bits
-        rxE(rxE<0) = 0;
-        rxE(rxE>M-1) = M-1;
-        
-        rxE = round(rxE);
-        rxE = rxE(numTrain+1:end); %no train
-        rxMSGE = msg2bits(rxE, M);
-        %}
+        rxMSGE = msg2bits(rx, M);
+        rxMSG2 = msg2bits(rx2,M);
         % Compute and store the EQUALIZER BER for this iteration
         % We're interested in the BER, which is the 2nd output of BITERR
         
-        %bits_notrain = bits((numTrain)+1:end);
-        rxMSGE_notrain = rxMSGE(numTrain+1:end);
-        bits_notrain = bits(numTrain+1:end);
-        [zzz, berVecENT(i,j)] = biterr(bits_notrain, rxMSGE_notrain); 
         
         
         % Compute and store the BER for this iteration
         % We're interested in the BER, which is the 2nd output of BITERR
-        [zzz, berVecE(i,j)] = biterr(bits, rxMSGE); 
-        [zzz, berVec(i,j)] = biterr(bits, rxMSG); 
-        
+        [zzz, berVecE(i,j)] = biterr(bits(numTrain:end), rxMSGE(numTrain:end)); 
+        [zzz, berVec(i,j)] = biterr(bits,rxMSG2);
     end  % End SNR iteration
 end      % End numIter iteration
 
 
 % Compute and plot the mean EQUALIZER BER
 berE = mean(berVecE,1);
-
-% Compute and plot the mean BER
-ber = mean(berVec,1);
-berNT = mean(berVecENT,1);
-
+ber2 = mean(berVec,1);
 figure
 semilogy(SNR_Vec, berE, 'DisplayName', 'Equalize')
-
 hold on
-semilogy(SNR_Vec, ber, 'DisplayName', 'BER')
-hold on
-semilogy(SNR_Vec, berNT, 'DisplayName', 'No TrainBER')
+semilogy(SNR_Vec, ber2, 'DisplayName', 'No equalize')
 
 % Compute the theoretical BER for this scenario
 % NOTE: there is no theoretical BER when you have a multipath channel
